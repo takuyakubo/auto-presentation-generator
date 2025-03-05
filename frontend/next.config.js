ca42690fabd188934bc0931855a4a3a908de4ba8
Takuya Kubo <takuyakubo2010@gmail.com>
2025-03-05 17:55:17 +0900

    Next.js設定にCORSヘッダーを追加

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,6 +12,19 @@ const nextConfig = {
       },
     ];
   },
+  // CORSヘッダーを追加
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
